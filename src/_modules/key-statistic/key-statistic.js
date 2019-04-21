41d96dd3a38942442de4fe2e194f8abe3d71210f
Fransiska Narulita <framelita@gmail.com>
2019-04-22 02:23:37 +0800

    added key statistic

---
+++ src/_modules/key-statistic/key-statistic.js
@@ -0,0 +1,8 @@
+'use strict';
+
+export default class KeyStatistic {
+  constructor() {
+    this.name = 'key-statistic';
+    console.log('%s module', this.name.toLowerCase());
+  }
+}
---
+++ src/_modules/key-statistic/tests/key-statistic.test.js
@@ -0,0 +1,15 @@
+'use strict';
+
+import KeyStatistic from '../key-statistic';
+
+describe('KeyStatistic View', function() {
+
+  beforeEach(() => {
+    this.keyStatistic = new KeyStatistic();
+  });
+
+  it('Should run a few assertions', () => {
+    expect(this.keyStatistic);
+  });
+
+});
