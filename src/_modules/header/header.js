e2b1d4954382196f72e326055bf95ffcaa6d75cc
Fransiska Narulita <framelita@gmail.com>
2019-04-22 11:57:17 +0800

    added scroll to hashtag on header

--- src/_modules/header/header.js
+++ src/_modules/header/header.js
@@ -11,5 +11,30 @@ export default class Header {
       .on('mouseleave', function() {
         $('.header__item').removeClass('is-show');
       });
+
+    $('.child__link').on('click', function(e) {
+      e.preventDefault();
+
+      let $this = $(this),
+        href = $this.attr('href'),
+        hashtag = href.split('#').pop();
+      console.log(hashtag, href, window.location.pathname, href.indexOf(window.location.pathname.split('/')[1]));
+      if (href.indexOf(window.location.pathname.split('/')[1]) >= 0) {
+        //if url is for current page
+        let scrollTop = $(`#${hashtag}`).offset().top - $('.header').outerHeight();
+        $('html, body').animate({
+          scrollTop: scrollTop
+        }, 1000);
+      } else {
+        window.location = href;
+      }
+    });
+
+    if(window.location.hash && $(window.location.hash).length) {
+      let scrollTop = $(window.location.hash).offset().top - $('.header').outerHeight();
+      $('html, body').animate({
+        scrollTop: scrollTop
+      }, 1000);
+    }
   }
 }
