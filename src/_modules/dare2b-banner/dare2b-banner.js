869d5b0258d5a26f0a7de637680b96b5f59b1dc8
Fransiska Narulita <framelita@gmail.com>
2019-04-24 00:11:58 +0800

    added dare2b banner. added images

---
+++ src/_modules/dare2b-banner/dare2b-banner.js
@@ -0,0 +1,8 @@
+'use strict';
+
+export default class Dare2bBanner {
+  constructor() {
+    this.name = 'dare2b-banner';
+    console.log('%s module', this.name.toLowerCase());
+  }
+}
---
+++ src/_modules/dare2b-banner/tests/dare2b-banner.test.js
@@ -0,0 +1,15 @@
+'use strict';
+
+import Dare2bBanner from '../dare2b-banner';
+
+describe('Dare2bBanner View', function() {
+
+  beforeEach(() => {
+    this.dare2bBanner = new Dare2bBanner();
+  });
+
+  it('Should run a few assertions', () => {
+    expect(this.dare2bBanner);
+  });
+
+});
